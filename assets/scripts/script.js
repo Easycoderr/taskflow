5b0eb35bb86a9e6833dfbfe39d6816e18673e4a2
Rauf <kurdrauf74@gmail.com>
2026-01-08 23:40:48 +0300

    add hamburger menu for mobile and tablets view screen.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,11 @@
 const heroSectionEl = document.getElementById("hero");
 const headerEl = document.getElementById("header");
+const menuBtnEl = document.getElementById("menu-btn");
+const mobileNavEl = document.getElementById("mobile-nav");
 
 const Observer = new IntersectionObserver(([entry]) => {
   if (!entry.isIntersecting) {
-    headerEl.classList.add(
-      "sticky",
-      "top-0",
-      "z-50",
-      "bg-white/50",
-      "backdrop-blur-sm"
-    );
+    headerEl.classList.add("sticky", "top-0", "z-50", "bg-white", "border-b");
   } else {
     headerEl.classList.remove("sticky", "top-0", "z-50", "bg-white");
   }
@@ -18,10 +14,14 @@ Observer.observe(heroSectionEl);
 
 // detect scrolling for give header sticky
 let lastScrollTop = 0;
-const navbar = document.getElementById("header"); // Example: element to hide/show
+
 window.addEventListener(
   "scroll",
   function () {
+    if (!mobileNavEl.classList.contains("hidden")) {
+      mobileNavEl.classList.add("hidden");
+      toggleMenuBtnIcon();
+    }
     // Get the current vertical scroll position
     const st = window.pageYOffset || document.documentElement.scrollTop;
 
@@ -29,12 +29,10 @@ window.addEventListener(
       // Scrolling Down
       headerEl.classList.add("sticky", "top-0", "z-50", "bg-white");
       // Example action: Hide a fixed header
-      // navbar.style.marginTop = '-100px';
     } else if (st < lastScrollTop) {
       // Scrolling Up
       headerEl.classList.remove("sticky", "top-0", "z-50", "bg-white");
       // Example action: Show a fixed header
-      // navbar.style.marginTop = '0px';
     }
 
     // Update the last scroll position for the next event
@@ -43,3 +41,22 @@ window.addEventListener(
   },
   false
 );
+// toggle menu
+menuBtnEl.addEventListener("click", (e) => {
+  mobileNavEl.classList.toggle("hidden");
+  toggleMenuBtnIcon();
+});
+mobileNavEl.addEventListener("click", () => {
+  mobileNavEl.classList.add("hidden");
+  toggleMenuBtnIcon();
+});
+
+// toggle menu btn icon
+
+function toggleMenuBtnIcon() {
+  if (mobileNavEl.classList.contains("hidden")) {
+    menuBtnEl.innerHTML = "<i class='bx  bxs-menu' style='color:#1e1e1e'></i>";
+  } else {
+    menuBtnEl.innerHTML = "<i class='bx  bxs-x' style='color:#1e1e1e'></i>";
+  }
+}
