0e865be0fd7519cf29aba57eb295b37b099bc0e5
Rauf <kurdrauf74@gmail.com>
2026-01-16 10:24:50 +0300

    refactor: making code cleaner & reusable, add outside click for sidebar.

--- src/UI/Layout.jsx
+++ src/UI/Layout.jsx
@@ -6,22 +6,38 @@ import { SiBlueprint } from "react-icons/si";
 import { FaTasks } from "react-icons/fa";
 import { GoSidebarCollapse, GoSidebarExpand } from "react-icons/go";
 import SimpleButtonIcon from "../components/SimpleButtonIcon";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import UseOutSideClicker from "../hooks/useOutSideClicker";
 
 function Layout() {
   const [isExpand, setIsExpand] = useState(false);
+  const refEl = useRef();
+  function handleExpanding() {
+    setIsExpand((expand) => !expand);
+  }
+  // Outside click sidebar
+  UseOutSideClicker(refEl, () => setIsExpand(false), true);
   return (
     <div
-      className={`grid md:grid-cols-[260px_1fr] ${isExpand ? "grid-cols-[200px_1fr]" : "grid-cols-[70px_1fr]"} overflow-x-visible 
+      className={`relative grid md:grid-cols-[250px_1fr] ${isExpand ? "grid-cols-[200px_1fr]" : "grid-cols-[70px_1fr]"} overflow-x-visible 
 grid-rows-[70px_1fr] min-h-screen bg-bg dark:bg-bg-dark transition-all duration-300`}
     >
+      {isExpand && (
+        <div className="bg-gray-50/30 dark:bg-gray-600/20 z-10 backdrop-blur-sm min-h-screen w-screen absolute top-0 bottom-0 right-0 left-0"></div>
+      )}
       {/* top bar */}
-      <div className="bg-bg dark:bg-bg-dark col-span-3 col-start-2 border-b border-gray-200"></div>
+
+      <div className="bg-bg dark:bg-bg-dark col-span-3 col-start-2 border-b border-gray-200 dark:border-gray-700"></div>
+
       {/* side bar */}
-      <aside className="relative bg-bg dark:bg-bg-dark min-h-screen text-text dark:text-text-dark col-span-1 row-start-1 row-span-4 border-r border-gray-200">
-        <div className="flex flex-col gap-3 min-h-screen">
+      <aside
+        aria-expanded={isExpand}
+        ref={refEl}
+        className={`z-30 bg-bg dark:bg-bg-dark min-h-screen text-text dark:text-text-dark col-span-1 row-start-1 row-span-4 border-r border-gray-200 dark:border-gray-700`}
+      >
+        <div className="relative flex flex-col gap-3 min-h-screen">
           <div className="md:hidden absolute top-18 -right-2.5 text-xl">
-            <SimpleButtonIcon onClick={() => setIsExpand((expand) => !expand)}>
+            <SimpleButtonIcon onClick={handleExpanding}>
               {isExpand ? (
                 <GoSidebarExpand className="group-hover:text-primary transition-all duration-400" />
               ) : (
@@ -36,74 +52,34 @@ grid-rows-[70px_1fr] min-h-screen bg-bg dark:bg-bg-dark transition-all duration-
             <Logo />
           </div>
           <ul className="flex flex-col gap-4 flex-1 overflow-hidden">
-            <li className={`group md:px-4 ${isExpand ? "px-4" : "px-2"}`}>
-              <a
-                href="#"
-                className={`whitespace-nowrap group-hover:from-primary/50 bg-gray-100 dark:bg-gray-600 bg-300 group-hover:animate-gradient group-hover:to-secondary/50 bg-linear-to-tl transition-all duration-300 md:px-4 py-4 rounded-md flex items-center justify-center md:justify-start md:gap-6 ${isExpand && "px-4 justify-start gap-6"}`}
-              >
-                <LuLayoutDashboard
-                  size={19}
-                  className="group-hover:scale-110 group-hover:-rotate-6 transition-all duration-300"
-                />
-                <span className={`md:block ${isExpand ? "block" : "hidden"}`}>
-                  Dashboard
-                </span>
-              </a>
-            </li>
-            <li className={`group md:px-4 ${isExpand ? "px-4" : "px-2"}`}>
-              <a
-                href="#"
-                className={`group-hover:from-primary/50 bg-gray-100 dark:bg-gray-600 bg-300 group-hover:animate-gradient group-hover:to-secondary/50 bg-linear-to-tl transition-all duration-300 md:px-4 py-4 rounded-md flex items-center justify-center md:justify-start md:gap-6 ${isExpand && "px-4 justify-start gap-6"}`}
-              >
-                <FaTasks
-                  size={19}
-                  className="group-hover:scale-110 group-hover:rotate-6 transition-all duration-300"
-                />
-                <span className={`md:block ${isExpand ? "block" : "hidden"}`}>
-                  Tasks
-                </span>
-              </a>
-            </li>
-            <li className={`group md:px-4 ${isExpand ? "px-4" : "px-2"}`}>
-              <a
-                href="#"
-                className={`group-hover:from-primary/50 bg-gray-100 dark:bg-gray-600 bg-300 group-hover:animate-gradient group-hover:to-secondary/50 bg-linear-to-tl transition-all duration-300 md:px-4 py-4 rounded-md flex items-center justify-center md:justify-start md:gap-6 ${isExpand && "px-4 justify-start gap-6"}`}
-              >
-                <SiBlueprint
-                  size={19}
-                  className="group-hover:scale-110 group-hover:-rotate-6 transition-all duration-300"
-                />{" "}
-                <span className={`md:block ${isExpand ? "block" : "hidden"}`}>
-                  Projects
-                </span>
-              </a>
-            </li>
-            <li className={`group md:px-4 ${isExpand ? "px-4" : "px-2"}`}>
-              <a
-                href="#"
-                className={`group-hover:from-primary/50 bg-gray-100 dark:bg-gray-600 bg-300 group-hover:animate-gradient group-hover:to-secondary/50 bg-linear-to-tl transition-all duration-300 md:px-4 py-4 rounded-md flex items-center justify-center md:justify-start md:gap-6 ${isExpand && "px-4 justify-start gap-6"}`}
-              >
-                <SlCalender className="group-hover:scale-110 group-hover:rotate-6 transition-all duration-300" />
-                <span className={`md:block ${isExpand ? "block" : "hidden"}`}>
-                  Calender
-                </span>
-              </a>
-            </li>
-            <li className={`group md:px-4 ${isExpand ? "px-4" : "px-2"}`}>
-              <a
-                href="#"
-                className={`group-hover:from-primary/50 bg-gray-100 dark:bg-gray-600  bg-300 group-hover:animate-gradient group-hover:to-secondary/50 bg-linear-to-tl transition-all duration-400 md:px-4 py-4 rounded-md flex items-center justify-center md:justify-start md:gap-6 ${isExpand && "px-4 justify-start gap-6"}`}
-              >
-                <SlSettings className="group-hover:-rotate-45 group-hover:scale-105 transition-all duration-400" />
-                <span className={`md:block ${isExpand ? "block" : "hidden"}`}>
-                  Settings
-                </span>
-              </a>
-            </li>
+            <SideBarItem isExpand={isExpand} label="Dashboard">
+              <LuLayoutDashboard
+                size={19}
+                className="group-hover:scale-110 group-hover:-rotate-6 transition-all duration-300"
+              />
+            </SideBarItem>
+            <SideBarItem isExpand={isExpand} label="Tasks">
+              <FaTasks
+                size={19}
+                className="group-hover:scale-110 group-hover:rotate-6 transition-all duration-300"
+              />
+            </SideBarItem>
+            <SideBarItem isExpand={isExpand} label="Projects">
+              <SiBlueprint
+                size={19}
+                className="group-hover:scale-110 group-hover:-rotate-6 transition-all duration-300"
+              />
+            </SideBarItem>
+            <SideBarItem isExpand={isExpand} label="Calender">
+              <SlCalender className="group-hover:scale-110 group-hover:rotate-6 transition-all duration-300" />
+            </SideBarItem>
+            <SideBarItem isExpand={isExpand} label="Settings">
+              <SlSettings className="group-hover:-rotate-45 group-hover:scale-105 transition-all duration-400" />
+            </SideBarItem>
             <li className="group mt-auto px-2 md:px-4 mb-5">
               <a
                 href="#"
-                className={`text-red-500 group-hover:from-red-300/50  bg-gray-100 dark:bg-gray-600 bg-300 group-hover:animate-gradient group-hover:via-red-200/60 group-hover:to-red-300/50 bg-linear-to-tl transition-all duration-400 md:px-4 py-4 rounded-md flex items-center justify-center md:justify-start md:gap-6 ${isExpand && "px-4 justify-start gap-6"}`}
+                className={`text-red-500 group-hover:from-red-300/50  bg-gray-100 dark:bg-gray-900 bg-300 group-hover:animate-gradient group-hover:via-red-200/60 group-hover:to-red-300/50 bg-linear-to-tl transition-all duration-400 md:px-4 py-4 rounded-md flex items-center justify-center md:justify-start md:gap-6 ${isExpand && "px-4 justify-start gap-6"}`}
               >
                 <LuLogOut className="group-hover:translate-x-0.5 group-hover:scale-105 transition-all duration-400" />
                 <span className={`md:block ${isExpand ? "block" : "hidden"}`}>
@@ -118,6 +94,21 @@ grid-rows-[70px_1fr] min-h-screen bg-bg dark:bg-bg-dark transition-all duration-
         main content
       </div>
     </div>
+  );
+}
+function SideBarItem({ isExpand, children, label }) {
+  return (
+    <li className={`group md:px-4 ${isExpand ? "px-4" : "px-2"}`}>
+      <a
+        href="#"
+        className={`group-hover:from-primary/50 bg-gray-100 dark:bg-gray-900  bg-300 group-hover:animate-gradient group-hover:to-secondary/50 bg-linear-to-tl transition-all duration-400 md:px-4 py-4 rounded-md flex items-center justify-center md:justify-start md:gap-6 ${isExpand && "px-4 justify-start gap-6"}`}
+      >
+        {children}
+        <span className={`md:block ${isExpand ? "block" : "hidden"}`}>
+          {label}
+        </span>
+      </a>
+    </li>
   );
 }
 
